bfcdb6e14e51110deda2ec9438fb4e2abe32f00b
atulwal <jaiswalatulya741@gmail.com>
2025-08-15 02:05:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 const apiKey = "253fb7580c920c8f18d7b8685e72fee7";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units = metric&q=bengaluru";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=raipur";
+const cityInput = document.querySelector(".search-input")
+const searchBtn = document.querySelector("search-icon")
 
-async function checkWeather() {
+
+async function getWeather(city) {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
+
+
+
+    
+}
+getWeather();
+
+function displayWeather(data) {
+    
+}
+
+function name(params) {
     
 }
