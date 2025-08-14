d31edc3ce15aa828f38eed5c4177d915c6304bea
atulwal <jaiswalatulya741@gmail.com>
2025-08-14 23:24:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const apiKey = "253fb7580c920c8f18d7b8685e72fee7";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units = metric&q=bengaluru";
+
+async function checkWeather() {
+    
+}
